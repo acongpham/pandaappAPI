040c4df21add5fef24c0f92c464376dd62c78044
acongpham <39377276+acongpham@users.noreply.github.com>
2019-11-04 17:09:26 +0700

    update
    code Addproduct activity

---
+++ UploadHinhAnh.php
@@ -0,0 +1,14 @@
+<?php
+include 'config/dbcon.php';
+include 'EntityClass.php';
+ $file_path="image/ImageProduct/";
+ $file_path=$file_path.basename($_FILES['upload_file']['name']);
+ if (move_uploaded_file($_FILES['upload_file']['tmp_name'],$file_path)){
+     echo $_FILES['upload_file']['name'];
+
+ } else{
+     echo "Failed";
+
+ }
+
+?>
