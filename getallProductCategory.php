a4cb1d725f1e1750ef6084b6fc06fe8ce5a3876d
acongpham <39377276+acongpham@users.noreply.github.com>
2019-11-18 17:30:23 +0700

    updatre
    dsa

--- getallProductCategory.php
+++ getallProductCategory.php
@@ -2,9 +2,10 @@
 include 'config/dbcon.php';
 include 'EntityClass.php';
 $key = $_POST['idcategory'];
+$key1=$_POST['offset'];
 $query = "select productId,name,price,product.discount,shop.shopName,shop.idShop,product.detail from Product 
 INNER JOIN shop on shop.idShop=product.idShop
-where idcategory='$key'";
+where idcategory='$key' ORDER BY product.productId DESC  LIMIT 15 OFFSET $key1";
 $data = mysqli_query($conn, $query);
 $array = array();
 while ($row = mysqli_fetch_assoc($data)) {
